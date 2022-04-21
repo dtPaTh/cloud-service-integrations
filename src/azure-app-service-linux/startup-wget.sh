51c0a90508b69f3169447d864b40be2fe7b5970f
Patrick <dtPaTh@users.noreply.github.com>
2022-04-21 11:10:40 +0200

    Update startup-wget.sh
    fix

--- src/azure-app-service-linux/startup-wget.sh
+++ src/azure-app-service-linux/startup-wget.sh
@@ -5,8 +5,10 @@
 #
 # #Optional env vars
 # DT_LINUX_FLAVOR ... Some containers may use alpine based images (e.g. tomcat) which need to set DT_LINUX_FLAVOR to 'musl'
-if [ -n "$DT_LINUX_FLAVOR" ]
+if [ -n "$DT_LINUX_FLAVOR" ] 
+then
     DT_LINUX_FLAVOR="default"
+fi
 
 wget -O /tmp/installer.sh "https://${DT_API_URL}/v1/deployment/installer/agent/unix/paas-sh/latest?Api-Token=${DT_API_TOKEN}&arch=x86&flavor=${DT_LINUX_FLAVOR}" 
 
